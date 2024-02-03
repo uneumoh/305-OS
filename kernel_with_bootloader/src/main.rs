4d201543e52cffcb116cd39132860358a5169ba8
Une Umoh <une.umoh@pau.edu.ng>
2024-02-03 15:25:03 +0100

    remastered

--- kernel_with_bootloader/src/main.rs
+++ kernel_with_bootloader/src/main.rs
@@ -156,13 +156,15 @@ fn my_entry_point(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
     interrupts::init();
 
     //Let's experience getting string from keyboard and saving into a variable for use
-    print!("Enter string: ");
-    let input = match input_str() {
-        Some(value) => value,
-        None => "".to_owned(),
-    };
-    println!("\nString entered is '{}'", input);
-
+    // print!("Enter string: ");
+    //let input = match input_str() {
+    //   Some(value) => value,
+    //  None => "".to_owned(),
+    //};
+    //println!("\nString entered is '{}'", input);
+    println!("Enter a string: ");
+    let user_input = input_str!();
+    println!("Your String is: {}", user_input);
     // invoke a breakpoint exception for test
     //x86_64::instructions::interrupts::int3();
 
